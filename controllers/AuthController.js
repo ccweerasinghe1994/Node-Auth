2738969a9d187d8f9fac3e29d2fa21c3a0ed7389
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-13 13:25:42 +0530

    saving to database

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,10 +1,17 @@
+const User = require('../models/User');
+
 const signup_get = (req, res) => {
     res.render('signup');
 }
-const signup_post = (req, res) => {
+const signup_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-    res.json(req.body);
+    try {
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error._message);
+    }
 }
 const login_get = (req, res) => {
     res.render('login');
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+        minlength: [6, 'Minimum password length is 6 characters'],
+    }
+});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
