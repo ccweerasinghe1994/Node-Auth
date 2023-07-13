cc9ca6b81e3ee419a731d0973cd0eb10deb8a28e
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-13 20:25:18 +0530

    final touches

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/AuthRoute');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
 const app = express();
 
 // middleware
@@ -21,8 +22,9 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch((err) => console.log("⛔⛔⛔⛔", err));
 
 // routes
+app.get('*', checkUser)
 app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // // cookies
--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 
 const router = Router();
-const { login_get, login_post, signup_get, signup_post } = require("../controllers/AuthController");
+const { login_get, login_post, signup_get, signup_post, logout_get } = require("../controllers/AuthController");
 router.get("/signup", signup_get);
 router.post("/signup", signup_post);
 router.get("/login", login_get);
 router.post("/login", login_post);
+router.get("/logout", logout_get);
 
 module.exports = router;
