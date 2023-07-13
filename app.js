7635b503782263b9efdd1eb1d2d3faadd64373f5
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-13 12:15:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb://127.0.0.1:27017/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  }))
+  .catch((err) => console.log("⛔⛔⛔⛔", err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
