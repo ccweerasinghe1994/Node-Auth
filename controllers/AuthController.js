9a220afd74023ba56e2ce93ddd83206de0e3955f
ccweerasinghe1994 <chamara.weerasinghe@my.sliit.lk>
2023-07-13 19:14:30 +0530

    login page

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,15 @@ const handleErrors = (err) => {
 
     let errors = { email: '', password: '' };
 
+    // incorrect email
+    if (err.message === 'incorrect email') {
+        errors.email = 'That email is not registered';
+    }
+    // incorrect password
+    if (err.message === 'incorrect password') {
+        errors.password = 'That password is incorrect';
+    }
+
     if (err.message.includes('user validation failed')) {
         Object.values(err.errors).forEach(({ properties }) => {
             errors[properties.path] = properties.message;
@@ -49,8 +58,17 @@ const signup_post = async (req, res) => {
 const login_get = (req, res) => {
     res.render('login');
 }
-const login_post = (req, res) => {
-    res.json(req.body);
+const login_post = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.login(email, password);
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(200).json({ user: user._id });
+    } catch (error) {
+        const errors = handleErrors(error);
+        res.status(400).json({ errors });
+    }
 }
 
 module.exports = {
